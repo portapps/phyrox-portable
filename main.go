ed04bd53f2fa4acff14737cc66ffa239d1f0484c
CrazyMax <crazy-max@users.noreply.github.com>
2019-03-18 16:22:40 +0100

    Handle notifications

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//go:generate go install -v github.com/kevinburke/go-bindata/go-bindata
+//go:generate go-bindata -prefix res/ -pkg assets -o assets/assets.go res/Firefox.lnk
 //go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 //go:generate goversioninfo -icon=res/papp.ico
 package main
@@ -7,10 +9,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 
+	"github.com/portapps/firefox-portable/assets"
 	. "github.com/portapps/portapps"
 	"github.com/portapps/portapps/pkg/dialog"
 	"github.com/portapps/portapps/pkg/mutex"
+	"github.com/portapps/portapps/pkg/shortcut"
+	"github.com/portapps/portapps/pkg/utl"
 )
 
 type config struct {
@@ -28,41 +34,45 @@ type policies struct {
 }
 
 var (
-	cfg = config{
+	app *App
+	cfg *config
+)
+
+func init() {
+	var err error
+
+	// Default config
+	cfg = &config{
 		Profile:               "default",
 		MultipleInstances:     false,
 		DisableTelemetry:      false,
 		DisableFirefoxStudies: false,
 	}
-)
 
-func init() {
-	Papp.ID = "firefox-portable"
-	Papp.Name = "Firefox"
-	InitWithCfg(&cfg)
+	// Init app
+	if app, err = NewWithCfg("firefox-portable", "Firefox", cfg); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
 }
 
 func main() {
-	Papp.AppPath = AppPathJoin("app")
-	Papp.DataPath = CreateFolder(AppPathJoin("data"))
+	utl.CreateFolder(app.DataPath)
+	profileFolder := utl.CreateFolder(app.DataPath, "profile", cfg.Profile)
 
-	profileFolder := CreateFolder(PathJoin(Papp.DataPath, "profile", cfg.Profile))
-
-	Papp.Process = PathJoin(Papp.AppPath, "firefox.exe")
-	Papp.WorkingDir = Papp.AppPath
-	Papp.Args = []string{
+	app.Process = utl.PathJoin(app.AppPath, "firefox.exe")
+	app.Args = []string{
 		"--profile",
 		profileFolder,
 	}
 
 	// Multiple instances
 	if cfg.MultipleInstances {
-		Log.Info("Multiple instances enabled")
-		Papp.Args = append(Papp.Args, "--no-remote")
+		Log.Info().Msg("Multiple instances enabled")
+		app.Args = append(app.Args, "--no-remote")
 	}
 
 	// Policies
-	distributionFolder := CreateFolder(PathJoin(Papp.AppPath, "distribution"))
+	distributionFolder := utl.CreateFolder(app.AppPath, "distribution")
 	policies := struct {
 		policies `json:"policies"`
 	}{
@@ -75,39 +85,70 @@ func main() {
 	}
 	rawPolicies, err := json.MarshalIndent(policies, "", "  ")
 	if err != nil {
-		Log.Fatal("Cannot marshal policies:", err)
+		Log.Fatal().Msg("Cannot marshal policies")
 	}
-	if err = ioutil.WriteFile(PathJoin(distributionFolder, "policies.json"), rawPolicies, 0644); err != nil {
-		Log.Fatal("Cannot write policies:", err)
+	if err = ioutil.WriteFile(utl.PathJoin(distributionFolder, "policies.json"), rawPolicies, 0644); err != nil {
+		Log.Fatal().Msg("Cannot write policies")
 	}
 
 	// Set env vars
-	OverrideEnv("MOZ_CRASHREPORTER", "0")
-	OverrideEnv("MOZ_CRASHREPORTER_DATA_DIRECTORY", CreateFolder(PathJoin(Papp.DataPath, "crashreporter")))
-	OverrideEnv("MOZ_CRASHREPORTER_DISABLE", "1")
-	OverrideEnv("MOZ_CRASHREPORTER_NO_REPORT", "1")
-	OverrideEnv("MOZ_DATA_REPORTING", "0")
-	OverrideEnv("MOZ_MAINTENANCE_SERVICE", "0")
-	OverrideEnv("MOZ_PLUGIN_PATH", CreateFolder(PathJoin(Papp.DataPath, "plugins")))
-	OverrideEnv("MOZ_UPDATER", "0")
+	crashreporterFolder := utl.CreateFolder(app.DataPath, "crashreporter")
+	pluginsFolder := utl.CreateFolder(app.DataPath, "plugins")
+	utl.OverrideEnv("MOZ_CRASHREPORTER", "0")
+	utl.OverrideEnv("MOZ_CRASHREPORTER_DATA_DIRECTORY", crashreporterFolder)
+	utl.OverrideEnv("MOZ_CRASHREPORTER_DISABLE", "1")
+	utl.OverrideEnv("MOZ_CRASHREPORTER_NO_REPORT", "1")
+	utl.OverrideEnv("MOZ_DATA_REPORTING", "0")
+	utl.OverrideEnv("MOZ_MAINTENANCE_SERVICE", "0")
+	utl.OverrideEnv("MOZ_PLUGIN_PATH", pluginsFolder)
+	utl.OverrideEnv("MOZ_UPDATER", "0")
 
 	// Create and check mutex
-	mu, err := mutex.New(Papp.ID, Log)
+	mu, err := mutex.New(app.ID)
 	defer mu.Release()
 	if err != nil {
 		if !cfg.MultipleInstances {
-			Log.Error("You have to enable multiple instances in your configuration if you want to launch multiple instances")
+			Log.Error().Msg("You have to enable multiple instances in your configuration if you want to launch another instance")
 			if _, err = dialog.MsgBox(
-				fmt.Sprintf("%s portable", Papp.Name),
-				fmt.Sprintf("You have to enable multiple instances in your configuration if you want to launch multiple instances of %s Portable", Papp.Name),
+				fmt.Sprintf("%s portable", app.Name),
+				"Other instance detected. You have to enable multiple instances in your configuration if you want to launch another instance.",
 				dialog.MsgBoxBtnOk|dialog.MsgBoxIconError); err != nil {
-				Log.Error("Cannot create dialog box", err)
+				Log.Error().Err(err).Msg("Cannot create dialog box")
 			}
 			return
 		} else {
-			Log.Warning("Another instance is already running:", err)
+			Log.Warn().Msg("Another instance is already running")
 		}
 	}
 
-	Launch(os.Args[1:])
+	// Copy default shortcut
+	shortcutPath := path.Join(os.Getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Firefox Portable.lnk")
+	defaultShortcut, err := assets.Asset("Firefox.lnk")
+	if err != nil {
+		Log.Error().Err(err).Msg("Cannot load asset Firefox.lnk")
+	}
+	err = ioutil.WriteFile(shortcutPath, defaultShortcut, 0644)
+	if err != nil {
+		Log.Error().Err(err).Msg("Cannot write default shortcut")
+	}
+
+	// Update default shortcut
+	err = shortcut.Create(shortcut.Shortcut{
+		ShortcutPath:     shortcutPath,
+		TargetPath:       app.Process,
+		Arguments:        shortcut.Property{Clear: true},
+		Description:      shortcut.Property{Value: "Firefox Portable by Portapps"},
+		IconLocation:     shortcut.Property{Value: app.Process},
+		WorkingDirectory: shortcut.Property{Value: app.AppPath},
+	})
+	if err != nil {
+		Log.Error().Err(err).Msg("Cannot create shortcut")
+	}
+	defer func() {
+		if err := os.Remove(shortcutPath); err != nil {
+			Log.Error().Err(err).Msg("Cannot remove shortcut")
+		}
+	}()
+
+	app.Launch(os.Args[1:])
 }
