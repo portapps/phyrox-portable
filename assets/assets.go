f20ee21052a60147dfdc4624c4be9a709b0f8fcd
CrazyMax <crazy-max@users.noreply.github.com>
2020-06-29 19:24:25 +0200

    Firefox 78.0

--- assets/assets.go
+++ assets/assets.go
@@ -183,6 +183,9 @@ var _bindata = map[string]func() (*asset, error){
 	"Firefox.lnk": firefoxLnk,
 }
 
+// AssetDebug is true if the assets were built with the debug flag enabled.
+const AssetDebug = false
+
 // AssetDir returns the file names below a certain
 // directory embedded in the file by go-bindata.
 // For example if you run go-bindata on data/... and data contains the
